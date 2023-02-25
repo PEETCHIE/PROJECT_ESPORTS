ae5a3f147ae59c3344f21c72855080c88552728a
PEETCHIE <chaianan.si@rmuti.ac.th>
2023-02-26 01:58:05 +0700

    UPDATE 24/02/2566 01:42

--- APP_ESPORTS/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from APP_ESPORTS.models import *
-
-# Register your models here.
-admin.site.register(TypeList)
-admin.site.register(Director)
-admin.site.register(AgeCategory)
-admin.site.register(Season)
-admin.site.register(Team)
